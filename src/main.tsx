445c93aea60e76db49128082cc8e84a182ee9bb7
yabselam <yabsirahoji@gmail.com>
2025-03-31 11:23:29 -0700

    deployment error solved

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { CartProvider } from "./context/cartProvider";
+import { CartProvider } from "./context/CartProvider";
 import { ProductsProvider } from "./context/ProductsProvider";
 
 
